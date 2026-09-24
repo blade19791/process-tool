fc66bf960487b01010f58082a89043922a011fab
ciech <ciecjames3@gmail.com>
2026-09-24 23:13:23 +0200

    add basic host validator to practice security

---
+++ validator.js
@@ -0,0 +1,11 @@
+export function isValidHostname(hostname) {
+  const hostnameRegex =
+    /^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9]))*$/;
+  return hostname.length <= 255 && hostnameRegex.test(hostname);
+}
+
+export function isValidIPv4(ip) {
+  const ipv4Regex =
+    /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+  return ipv4Regex.test(ip);
+}
